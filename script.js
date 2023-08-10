371a8d239ce2c722d89a158ec1b501039caef4ca
Darlington112 <Darlingtonevelyn112@gmail.com>
2023-08-10 06:40:34 +0100

    upddated

--- script.js
+++ script.js
@@ -13,3 +13,45 @@ if (close) {
         navBar.classList.remove('open')
     })
 }
+
+
+
+// search
+const search = () => {
+    const searchBar = document.getElementById('search-bar').value.toLowerCase();
+    const productList = document.getElementById('product-container')
+    const product = document.querySelectorAll('.product')
+    const productName = document.querySelectorAll('.product-name')
+    
+
+    for (let i = 0; i < productName.length; i++){
+
+        let match = product[i].querySelectorAll('.product-name')[0]
+
+        if (match) {
+            let textValue = match.textContent || match.innerHtml
+    
+            if (textValue.toLowerCase().indexOf(searchBar) > -1) {
+                product[i].style.display = '';
+            } else {
+                product[i].style.display = 'none';
+
+            }
+        }
+
+    }
+  
+}
+
+// Accordion
+const content = document.getElementsByClassName('contentBox')
+
+for (let i = 0; i < content.length; i++){
+    content[i].addEventListener('click', function () {
+        content[i].classList.toggle('show')
+    })
+}
+
+
+
+
